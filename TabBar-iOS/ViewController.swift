74e8937f6b1675333d79969bc612a07cf8962d29
Wim Sonevel <wim.sonevel@yahoo.co.id>
2016-07-27 20:53:50 +0700

    Initial Commit

---
+++ TabBar-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBar-iOS
+//
+//  Created by Docotel on 7/27/16.
+//  Copyright © 2016 Kwikku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
